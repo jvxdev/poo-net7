7b618b4ce7a41b9cf80c7a0a9c5e8a0f649b286b
João Victor Dias de Moraes <joaovictorddm@outlook.com>
2023-06-06 17:21:24 -0300

    Criação dos métodos construtores em Url e Campaign. Utilização de parâmetros opcionais.

--- UtmBuilder.Core/ValueObjects/Campaign.cs
+++ UtmBuilder.Core/ValueObjects/Campaign.cs
@@ -8,16 +8,32 @@ namespace UtmBuilder.Core.ValueObjects
 {
     public class Campaign : ValueObject
     {
-        public string Id { get; set; }
+        public Campaign(
+            string source,
+            string medium,
+            string name,
+            string? id = null,
+            string? term = null,
+            string? content = null)
+        {
+            Source = source;
+            Medium = medium;
+            Name = name;
+            Id = id;
+            Term = term;
+            Content = content;
+        }
 
-        public string Source { get; set; }
+        public string Source { get; }
 
-        public string Medium { get;set; }
+        public string Medium { get; }
 
-        public string Name { get; set; }
+        public string? Id { get; }
 
-        public string Term { get; set; }
+        public string Name { get; }
 
-        public string Content { get; set; }
+        public string? Term { get; }
+
+        public string? Content { get; }
     }
 }
--- UtmBuilder.Core/ValueObjects/Url.cs
+++ UtmBuilder.Core/ValueObjects/Url.cs
@@ -8,5 +8,11 @@ namespace UtmBuilder.Core.ValueObjects
 {
     public class Url : ValueObject
     {
+        public Url(string address) 
+        {
+            Address = address;
+        }
+
+        public string Address { get; }
     }
 }
