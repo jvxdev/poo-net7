36b96a4444f87b05161498ab089baef32fd29db4
João Victor Dias de Moraes <joaovictorddm@outlook.com>
2023-06-06 18:13:32 -0300

    Adição de sumário para descrição de propriedades e construtores de entidades.

--- UtmBuilder.Core/ValueObjects/Campaign.cs
+++ UtmBuilder.Core/ValueObjects/Campaign.cs
@@ -8,6 +8,15 @@ namespace UtmBuilder.Core.ValueObjects
 {
     public class Campaign : ValueObject
     {
+        /// <summary>
+        /// Generate a new campaign for a URL
+        /// </summary>
+        /// <param name="source">The referrer (e.g. google, newsletter)</param>
+        /// <param name="medium">Marketing medium (e.g. cpc, banner, email)</param>
+        /// <param name="name">Product, promo code, or slogan (e.g. spring_sale) One of campaign name or campaign id are required.</param>
+        /// <param name="id">The ads campaign id.</param>
+        /// <param name="term">Identify the paid keywords</param>
+        /// <param name="content">Use to differentiate ads</param>
         public Campaign(
             string source,
             string medium,
@@ -24,16 +33,34 @@ namespace UtmBuilder.Core.ValueObjects
             Content = content;
         }
 
-        public string Source { get; }
-
-        public string Medium { get; }
-
+        /// <summary>
+        /// The ads campaign id.
+        /// </summary>
         public string? Id { get; }
 
+        /// <summary>
+        /// The referrer (e.g. google, newsletter)
+        /// </summary>
+        public string Source { get; }
+
+        /// <summary>
+        /// Marketing medium (e.g. cpc, banner, email)
+        /// </summary>
+        public string Medium { get; }
+
+        /// <summary>
+        /// Product, promo code, or slogan (e.g. spring_sale) One of campaign name or campaign id are required.
+        /// </summary>
         public string Name { get; }
 
+        /// <summary>
+        /// Identify the paid keywords
+        /// </summary>
         public string? Term { get; }
 
+        /// <summary>
+        /// Use to differentiate ads
+        /// </summary>
         public string? Content { get; }
     }
 }
