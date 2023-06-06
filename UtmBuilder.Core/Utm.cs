fb979e0c093f0a252c87e285574464a2e2c76e7a
João Victor Dias de Moraes <joaovictorddm@outlook.com>
2023-06-06 18:22:13 -0300

    Criação do construtor na entidade Utm e alteração das propriedades para readonly.

--- UtmBuilder.Core/Utm.cs
+++ UtmBuilder.Core/Utm.cs
@@ -3,7 +3,20 @@
 namespace UtmBuilder.Core;
 public class Utm
 {
-    public Url Url { get; set; }
+    public Utm(Url url, Campaign campaign)
+    {
+        Url = url;
+        Campaign = campaign;
+    }
+
+    /// <summary>
+    /// URL (Website Link)
+    /// </summary>
+
+    public Url Url { get; }
     
-    public Campaign Campaign { get; set; }
+    /// <summary>
+    /// Campaign Details
+    /// </summary>
+    public Campaign Campaign { get; }
 }
