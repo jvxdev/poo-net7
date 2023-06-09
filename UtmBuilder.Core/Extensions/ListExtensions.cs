0c07548319c418cea75088db5075e108e5282332
João Victor Dias de Moraes <joaovictorddm@outlook.com>
2023-06-09 08:29:47 -0300

    Implementação do método de extensão 'AddIfNotNull' de uma List.

--- UtmBuilder.Core/Extensions/ListExtensions.cs
+++ UtmBuilder.Core/Extensions/ListExtensions.cs
@@ -8,9 +8,15 @@ namespace UtmBuilder.Core.Extensions
 {
     public static class ListExtensions
     {
-        public static void AddIfNotNull(this List<string> list)
+        public static void AddIfNotNull(
+            this List<string> list,
+            string key,
+            string? value)
         {
-
+            if (!string.IsNullOrEmpty(value))
+            {
+                list.Add($"{key}={value}");
+            }
         }
     }
 }
